f7ee1dc29f6f92233d9664d2521830e40bb24428
MattCornicDev <glgharib@gmail.com>
2026-08-26 13:33:32 +0200

    MAJ dependances
    Signed-off-by: MattCornicDev <glgharib@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { Inter } from "@next/font/google";
+import { Inter } from "next/font/google";
 import { Archivo_Narrow } from "next/font/google";
 
 const archivo = Archivo_Narrow({
